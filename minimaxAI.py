b45b5918009fe46ab2d6642c061532a59a4faac1
mrazzsolt <85168335+mrazzsolt@users.noreply.github.com>
2025-01-31 12:57:32 +0100

    Update minimaxAI.py

--- minimaxAI.py
+++ minimaxAI.py
@@ -1,24 +1,27 @@
 import copy
+import checks as c
 
-def make_move(board, inp, xOrY):
-    intinp = [int(x) for x in inp]
-    board[intinp[0]][intinp[1]] = xOrY
+def execute_move(grid, position, symbol):
+    int_position = [int(x) for x in position]
+    grid[int_position[0]][int_position[1]] = symbol
 
-def get_winner(board):
-    all_line_co_ords = get_all_line_co_ords()
-    for line in all_line_co_ords:
-        line_values = [board[x][y] for (x, y) in line]
-        if len(set(line_values)) == 1 and line_values[0] is not None:
-            return line_values[0]
-    return None
+# Függvény a győztes ellenőrzésére
+def determine_winner(grid):
+    all_lines = generate_all_lines()  # Az összes lehetséges nyerő vonal koordinátái
+    for line in all_lines:
+        values = [grid[x][y] for (x, y) in line]  # Kigyűjtjük a vonal értékeit
+        if len(set(values)) == 1 and values[0] is not None:
+            return values[0]
+    return None  # Ha nincs győztes, None-t adunk vissza
 
-def get_all_line_co_ords():
-    cols = []
+# Függvény az összes lehetséges nyerő vonal generálására
+def generate_all_lines():
+    columns = []
     for x in range(0, 3):
-        col = []
+        column = []
         for y in range(0, 3):
-            col.append((x, y))
-        cols.append(col)
+            column.append((x, y))
+        columns.append(column)
 
     rows = []
     for y in range(0, 3):
@@ -31,72 +34,66 @@ def get_all_line_co_ords():
         [(0, 0), (1, 1), (2, 2)],
         [(0, 2), (1, 1), (2, 0)]
     ]
-    return cols + rows + diagonals
+    return columns + rows + diagonals
 
-def is_board_full(board):
-    for col in board:
-        for sq in col:
-            if sq is None:
-                return False
-    return True
-
-def getAllLegalMove(board):
-    indexes = []
-    for index, cell in enumerate([elem for row in board for elem in row]):
-        if cell is None:
-            indexes.append(index)
-    return indexes
-
-def minimax(board, markAI):
-    best_move = None
+# Függvény a legjobb lépés megtalálására az AI számára
+def minimax(grid, ai_symbol):
+    best_action = None
     best_score = None
-    num_columns = len(board[0])
-    legalmoves = []
-    indexes = getAllLegalMove(board)
-    for x in indexes:
-        row = x // num_columns
-        col = x % num_columns
-        legalmoves.append([row, col])
+    num_cols = len(grid[0])
+    possible_actions = []
 
-    for move in legalmoves:
-        _board = copy.deepcopy(board)
-        make_move(_board, move, markAI)  # Make move on the copied board
-        opp = 'X' if markAI == 'O' else 'O'  # Alternate opponent
-        score = _minimax_score(_board, opp, markAI)
-        if best_score is None or score > best_score:
-            best_move = move
+    # Az összes lehetséges lépés koordinátáinak kiszámítása
+    valid_indices = c.getAllLegalMove(grid)
+    for index in valid_indices:
+        row = index // num_cols
+        col = index % num_cols
+        possible_actions.append([row, col])
+
+    # Minden lehetséges lépés kiértékelése
+    for action in possible_actions:
+        temp_grid = copy.deepcopy(grid)  # Másolat készítése a tábláról
+        execute_move(temp_grid, action, ai_symbol)  # Lépés végrehajtása a másolaton
+        opponent_symbol = 'X' if ai_symbol == 'O' else 'O'
+        score = evaluate_move(temp_grid, opponent_symbol, ai_symbol)  # Pontszám kiszámítása
+        if best_score is None or score > best_score:  # Ha ez a lépés jobb, mint az eddigi legjobb
+            best_action = action
             best_score = score
 
-    return best_move
+    return best_action
 
-def _minimax_score(board, player_to_move, player_to_optimize):
-    legalmoves = []
-    num_columns = len(board[0])
-    winner = get_winner(board)
-    
+# Függvény a lépés pontszámának kiszámítására
+def evaluate_move(grid, current_player, target_player):
+    possible_actions = []
+    num_cols = len(grid[0])
+    winner = determine_winner(grid)
+
+    # Ha van győztes, visszaadjuk a megfelelő pontszámot
     if winner is not None:
-        if winner == player_to_optimize:
-            return 10
+        if winner == target_player:
+            return 10  # Ha a céljátékos nyert, 10 pont
         else:
-            return -10
-    elif is_board_full(board):
-        return 0
+            return -10  # Ha az ellenfél nyert, -10 pont
+    elif c.isBoardFull(grid):
+        return 0  # Döntetlen esetén 0 pont
 
-    indexes = getAllLegalMove(board)
-    for x in indexes:
-        row = x // num_columns
-        col = x % num_columns
-        legalmoves.append([row, col])
+    # Az összes lehetséges lépés koordinátáinak kiszámítása
+    valid_indices = c.getAllLegalMove(grid)
+    for index in valid_indices:
+        row = index // num_cols
+        col = index % num_cols
+        possible_actions.append([row, col])
 
-    scores = []
-    for move in legalmoves:
-        _board = copy.deepcopy(board)
-        make_move(_board, move, player_to_move)
-        opp = 'X' if player_to_move == 'O' else 'O'
-        opp_best_response_score = _minimax_score(_board, opp, player_to_optimize)
-        scores.append(opp_best_response_score)
+    scores = []  # Az összes lehetséges lépés pontszámai
+    for action in possible_actions:
+        temp_grid = copy.deepcopy(grid)  # Másolat készítése a tábláról
+        execute_move(temp_grid, action, current_player)  # Lépés végrehajtása a másolaton
+        opponent_symbol = 'X' if current_player == 'O' else 'O'
+        opponent_score = evaluate_move(temp_grid, opponent_symbol, target_player)  # Ellenfél pontszáma
+        scores.append(opponent_score)
 
-    if player_to_move == player_to_optimize:
+    # Ha a céljátékos van soron, a maximális pontszámot választjuk
+    if current_player == target_player:
         return max(scores)
-    else:
+    else:  # Ha az ellenfél van soron, a minimális pontszámot választjuk
         return min(scores)
