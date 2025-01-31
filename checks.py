6dc0a82b271e45f4902cb8abc5d9103990852d33
mrazzsolt <85168335+mrazzsolt@users.noreply.github.com>
2025-01-31 12:58:44 +0100

    Update checks.py

--- checks.py
+++ checks.py
@@ -1,64 +1,55 @@
-class checks():  
-    def __init__(self, board):
-        self.board = board
-        self.board_height = 3
-        self.board_width = 3
-    
-    def getAllLegalMove(self,board):
-        indexes = []
-        for index, cell in enumerate([elem for row in board for elem in row]):
-            if cell is None:
-                indexes.append(index)
-        return indexes
+def getAllLegalMove(board):
+    indexes = []
+    for index, cell in enumerate([elem for row in board for elem in row]):
+        if cell is None:
+            indexes.append(index)
+    return indexes
 
-    def isLegalMove(self, step):
-        print(self.board)
-        intStep = []
-        for x in step:
-            intStep.append(int(x))
-        if len(intStep) > 2:
-            print('Invalid step!')
-            return False
-        if intStep[0]+1 > self.board_height or intStep[1]+1 > self.board_width:
-            print('Invalid step!')
-            return False 
-        if self.board[intStep[0]][intStep[1]] is None:
-            return True
-        else:
-            print('Invalid step!')
-            return False
-    
-    def isBoardFull(self):
-        for x in self.board:
-            for y in x:
-                if y is None:
-                    return False
-        return True
-    
-    def checkRow(self, lst):
-        return lst[0] is not None and all(x == lst[0] for x in lst)
-
-    def checkColumn(self, board, col_index):
-        column_values = [row[col_index] for row in board]
-        return column_values[0] is not None and all(val == column_values[0] for val in column_values)
-    
-    def checkPrimaryDiagonal(self):
-        return self.board[0][0] is not None and all(self.board[i][i] == self.board[0][0] for i in range(len(self.board)))
-
-    def checkSecondaryDiagonal(self):
-        n = len(self.board)
-        return self.board[0][n-1] is not None and all(self.board[i][n-i-1] == self.board[0][n-1] for i in range(n))
-   
-    def getWinner(self):
-        for x in self.board:
-            if self.checkRow(x):
-                return True
-        
-        for x in range(3):
-            if self.checkColumn(self.board, x):
-                return True
-        if self.checkPrimaryDiagonal():
-            return True
-        if self.checkSecondaryDiagonal():
-            return True
+def isLegalMove(board, step):
+    intStep = [int(x) for x in step]
+    if len(intStep) > 2:
+        print('Invalid step!')
         return False
+    if intStep[0] + 1 > 3 or intStep[1] + 1 > 3:
+        print('Invalid step!')
+        return False
+    if board[intStep[0]][intStep[1]] is None:
+        return True
+    else:
+        print('Invalid step!')
+        return False
+
+def isBoardFull(board):
+    for x in board:
+        for y in x:
+            if y is None:
+                return False
+    return True
+
+def checkRow(lst):
+    return lst[0] is not None and all(x == lst[0] for x in lst)
+
+def checkColumn(board, col_index):
+    column_values = [row[col_index] for row in board]
+    return column_values[0] is not None and all(val == column_values[0] for val in column_values)
+
+def checkPrimaryDiagonal(board):
+    return board[0][0] is not None and all(board[i][i] == board[0][0] for i in range(len(board)))
+
+def checkSecondaryDiagonal(board):
+    n = len(board)
+    return board[0][n-1] is not None and all(board[i][n-i-1] == board[0][n-1] for i in range(n))
+
+def getWinner(board):
+    for x in board:
+        if checkRow(x):
+            return True
+
+    for x in range(3):
+        if checkColumn(board, x):
+            return True
+    if checkPrimaryDiagonal(board):
+        return True
+    if checkSecondaryDiagonal(board):
+        return True
+    return False
